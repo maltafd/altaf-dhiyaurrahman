ef867096d21f5aa34ef9802b74f038aeeeab8a8b
maltafd <maltafd@student.telkomuniversity.ac.id>
2020-09-19 21:42:19 +0800

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("nama : M. Altaf Dhiyaurrahman");
+        System.out.println("kelas : D3IF-44-04");
+        System.out.println("NIM : 6706202134");
+        System.out.println("Hello Altaf");
+    }
+}
